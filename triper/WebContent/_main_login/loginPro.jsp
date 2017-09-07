bef15f5a58557257d6fb1cf2eefdbf390ba32a85
kosta <kosta@192.168.0.125>
2017-09-07 17:12:39 +0900

    asdf

--- triper/WebContent/_main_login/loginPro.jsp
+++ triper/WebContent/_main_login/loginPro.jsp
@@ -7,18 +7,18 @@
 
 <fmt:requestEncoding value="utf-8"/>
 
-<meta http-equiv="Refresh"  content="0; url=loginForm.do ">
-
-<c:if test="${ check == 1 }">
+<c:if test="${ check == 0 }">
 <script type="text/javascript">
 alert("비밀번호 틀림");
 history.go(-1); 
 </script>	
 </c:if>
 
-<c:if test="${ check == 0 }">
+<c:if test="${ check == -1 }">
 <script type="text/javascript">
 alert("아이디가 없음");
 history.go(-1); 
 </script>	
 </c:if>
+
+<meta http-equiv="Refresh"  content="0; url=loginForm.do ">
--- triper/src/kosta/rental/loginAction/LoginProAction.java
+++ triper/src/kosta/rental/loginAction/LoginProAction.java
@@ -2,25 +2,72 @@ package kosta.rental.loginAction;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import kosta.rental.loginModel.RentalDAO;
 
-public class LoginProAction implements CommandAction{
-	@Override
-	public String process(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
-    	request.setCharacterEncoding("UTF-8");
-    	System.out.println("sdf");
-    	String id = request.getParameter("id");
-        String pwd = request.getParameter("password");
-        RentalDAO dao = RentalDAO.getInstance();
-        int check = dao.userCheck(id, pwd);
-        
-		return "/_main_login/loginPro.jsp";
-	}	
+/**
+ * Servlet implementation class ReviewController
+ */
+@WebServlet("*.do")
+public class LoginProAction extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginProAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      System.out.println("get");
+      actionDo(request, response);
+   }
+
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      System.out.println("post");
+      actionDo(request, response);
+   
+   }
+
+   private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+      String viewPage = null;
+      String uri = request.getRequestURI();
+      String conPath = request.getContextPath();
+      String com = uri.substring(conPath.length());
+      int check = 0;
+      if(com.equals("/_main_login/loginPro.do")){
+    	  String id = request.getParameter("id");
+          String pwd = request.getParameter("password");
+    	  RentalDAO dao = RentalDAO.getInstance();
+    	  try {
+			check = dao.userCheck(id, pwd);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+         viewPage="/_main_login/loginPro.jsp";
+      } else if(com.equals("/_main_login/loginForm.do")) {
+    	  viewPage="/_main_login/index.jsp";
+      }
+      System.out.println(viewPage);
+      RequestDispatcher dp = request.getRequestDispatcher(viewPage);
+      request.setAttribute("check", check);
+      dp.forward(request, response);
+      
+   }
+
 }
